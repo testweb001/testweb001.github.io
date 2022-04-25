958df07f36c963212e794b70c721be41a785a904
testweb001 <104340642+testweb001@users.noreply.github.com>
2022-04-25 14:03:32 +0900

    Update config.py

--- builder/config.py
+++ builder/config.py
@@ -4,4 +4,4 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BUILD_PATH = os.path.join(BASE_PATH, 'docs')
 API_KEY = os.getenv('INPUT_API_KEY', '') or 'AIzaSyC6dzKyPLK2TsfjiWtxytUE7LQEr64QPwc'
 
-DATA_URL = 'https://docs.google.com/spreadsheets/d/1EDLlUuY2Ia5MKNbCTOftxxSxBaK3C9pRFOIUvMY30eY/edit?usp=sharing'
+DATA_URL = 'https://docs.google.com/spreadsheets/d/1f1RgJPGpLSaITRYJjHg4mkQuQmwdsObdfQ4BsOFn45Q/edit?usp=sharing'
